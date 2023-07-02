8ca841cc36ac9ae8e6b546730830c48a1c0668ef
Joey <headmastereggy@gmail.com>
2023-07-02 16:28:27 +0800

    changes

--- src/main/java/com/itheima/dao/impl/EmpDaoA.java
+++ src/main/java/com/itheima/dao/impl/EmpDaoA.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class EmpDaoA implements EmpDao {
     @Override
     public List<Emp> listEmp() {
-        //1. 加载并解析emp.xml
+        //加载并解析emp.xml
         String file = this.getClass().getClassLoader().getResource("emp.xml").getFile();
         System.out.println(file);
         List<Emp> empList = XmlParserUtils.parse(file, Emp.class);
